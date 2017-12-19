d7264fe0bca06d283872c0e49e6e920a34f4ac50
PrafulSingha <34261487+PrafulSingha@users.noreply.github.com>
2017-12-19 10:41:37 +0530

    update

---
+++ src/com/ace/exception/SDApplicationException.java
@@ -0,0 +1,33 @@
+package com.ace.exception;
+
+public class SDApplicationException extends Exception{
+
+	public SDApplicationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SDApplicationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SDApplicationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SDApplicationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SDApplicationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
